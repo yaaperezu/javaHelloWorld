fa312cdc309c26afe17b7b16e058d3fa0bb174e9
Yasser Pérez <yasserperez10@gmail.com>
2020-10-08 08:10:15 -0500

    Integración con webhooks

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-        System.out.println("Hello, World 2i");
+        System.out.println("Hello, World Yasser curso de microservicios");
   }
 }
